d572619a4d645a41ab70eb4eb1114ff8ff0e689b
EmilyFerreira10 <emilyferreira@alunos.utfpr.edu.br>
2025-04-09 08:33:09 -0300

    Add files via upload

---
+++ notas_2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    float n1, n2, n3, media;
+    float frequencia;
+
+    printf ("digite as tres notas:");
+    scanf("%f %f %f", &n1, &n2, &n3);
+
+    media = (n1 + n2 + n3)/3;
+
+    printf ("digite a frequencia do estudante:");
+    scanf("%f", &frequencia);
+
+    if (media<0 || media > 10 || frequencia <0 || frequencia > 100){
+      printf("erro: valores inválidos. A media deve estar entre 0 e 10, e a frequencia entre 0%% e 100%%.\n");
+      return 1;
+    }
+
+    if (media>8.0 && frequencia >=75.0){
+    printf("aprovado com distinção.");
+    }
+    else if (media>=6.0 && frequencia >=75.0){
+        printf("aprovado direto.");
+    }
+    else if ((media>=4.0 && media < 6.0 && frequencia >=75.0) ||
+            (media>=6.0 &&  frequencia < 75.0)||
+            (media>=4.0 && frequencia > 50.0 && frequencia <75.0)){
+        printf("vai para a final.");
+    }
+    else
+        printf("reprovado direto.");
+{
+    printf("\n O estudante foi %s pois teve media %.1f e frequencia %.1f%%.\n",
+           categoria, media, frequencia);
+    return 0;
+}
